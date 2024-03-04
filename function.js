764ec8dcd832407f2a19a3ca3d2d3b4f76a802b1
madaswamym <madaswamy@gmail.com>
2024-03-04 12:28:26 +0530

    This is first commit

---
+++ function.js
@@ -0,0 +1,35 @@
+// function samsung(){
+//     console.log("This is a Samsung Phone")
+// }
+// function Nokia(){
+//     console.log("This is a NOkia Phone")
+// }
+// function iPhone(){
+//     console.log("This is a iPhone Phone")
+// }
+// function Xiomi(){
+//     console.log("This is a Xiomi Phone")
+// }
+
+
+// samsung()
+// Nokia()
+// iPhone()
+// Xiomi()
+
+
+// var a
+// var b
+
+// function add(a, b){
+//     total = (a+b)
+//     console.log("The total is "+total)
+// }
+
+// add(10,20)
+
+function area(a, b){
+    total = (a*b)
+    console.log("The area total is :"+total)
+}
+area(10,20)
